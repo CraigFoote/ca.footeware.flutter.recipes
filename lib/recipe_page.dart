f09bff27a5de9b2a474278e2264835ca977270ab
Craig Foote <CraigFoote@gmail.com>
2022-12-28 12:50:02 -0500

    pinch-zoom images finally

--- lib/recipe_page.dart
+++ lib/recipe_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:gesture_zoom_box/gesture_zoom_box.dart';
 import 'package:flutter_recipes/recipe.dart';
 
 class RecipePage extends StatelessWidget {
@@ -14,34 +15,35 @@ class RecipePage extends StatelessWidget {
         title: Text(recipe.name),
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 30),
-                child: SelectableText(
-                  recipe.body,
-                  style: const TextStyle(fontSize: 22.0),
-                ),
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 30),
+              child: SelectableText(
+                recipe.body,
+                style: const TextStyle(fontSize: 22.0),
               ),
-              for (String base64 in recipe.images) getImage(base64)
-            ],
-          ),
+            ),
+            const Text(
+              "Double tap to zoom",
+              style: TextStyle(
+                color: Colors.deepPurple,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            for (String base64 in recipe.images) getImage(base64)
+          ],
         ),
       ),
     );
   }
 
   getImage(String base64) {
-    return InteractiveViewer(
-      maxScale: 5.0,
-      minScale: 0.5,
-      child: Image.memory(
-        base64Decode(base64.substring(23)),
-      ),
-
-    );
+    return GestureZoomBox(
+        child: Image.memory(
+      base64Decode(base64.substring(23)),
+    ));
   }
 }
