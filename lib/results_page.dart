53a9604d46a029002d91cd4a71a578285fddd946
Craig Foote <CraigFoote@gmail.com>
2022-12-28 22:55:31 -0500

    remove search button on results page and add tags to recipe page

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter_recipes/recipe_page.dart';
-import 'package:flutter_recipes/search_page.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_recipes/recipe.dart';
